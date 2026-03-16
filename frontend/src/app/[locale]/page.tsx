6e916ead2f7612030532b7021acec69ea0e6b117
Truc Pham <trucpham04@proton.me>
2026-03-16 09:17:05 +0700

    refactor(fe): Update RootPage to redirect to dashboard and comment out Home component

--- frontend/src/app/[locale]/page.tsx
+++ frontend/src/app/[locale]/page.tsx
@@ -1,5 +1,7 @@
-import Home from "@/app/[locale]/home/page";
+// import Home from "@/app/[locale]/home/page";
+import { redirect } from "@/i18n/navigation";
 
 export default function RootPage() {
-  return <Home />;
+  // return <Home />;
+  redirect({ href: "/dashboard", locale: "vi" });
 }
