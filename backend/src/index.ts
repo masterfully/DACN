c8f8a98abe9aecec726d5052a6b71da7a7ecdc89
Truc Pham <trucpham04@proton.me>
2026-03-19 10:57:24 +0700

    feat(be): add CORS support to backend for cross-origin requests
    - include 'cors' package in dependencies
    - configure CORS middleware to allow requests from http://localhost:3000 with specified methods and headers

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,23 @@
 /// <reference path="./types/express.d.ts" />
 import "dotenv/config";
 import express from "express";
-import apiRouter from "./routes";
 import { errorHandler } from "./middleware/errorHandler";
 import { requestLogger } from "./middleware/requestLogger";
+import apiRouter from "./routes";
+
+var cors = require("cors");
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
+
 app.use(express.json());
 app.use(requestLogger);
 
